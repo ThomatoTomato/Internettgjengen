12488f40385fe6e95a890231c57344c33e754a4b
CelestialCry <thomas.w.thorbjornsen@gmail.com>
2020-10-08 15:36:17 +0200

    lager hovedside uten match og chat med Markus

---
+++ scripts/hovedside.js
@@ -0,0 +1,11 @@
+function back() {
+    document.getElementById("side2").style = "display: none;"
+    document.getElementById("hjerte").style = "display: none;"
+    document.getElementById("side1").style = "display: block;"
+  }
+
+function chat() {
+    document.getElementById("side1").style = "display:none;"
+    document.getElementById("side2").style = "display:inline-flex;"
+    document.getElementById("hjerte").style = "display:block"
+}
