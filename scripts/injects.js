3f7fecb43a72eaae3b80f110fcda84e52214105b
CelestialCry <thomas.w.thorbjornsen@gmail.com>
2020-10-22 19:07:15 +0200

    gjør klar til master

--- scripts/injects.js
+++ scripts/injects.js
@@ -7,7 +7,6 @@ if (window.sessionStorage.getItem("activeUser") == null)
         displayRegister(); 
     }
 }
-else if ( document.title == "Forside" ) {}
 else { displayHeartProfile(); }
 
 navigation();
