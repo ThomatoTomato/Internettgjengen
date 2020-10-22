83b091198710352699ce3e2a08bd731da660c2ac
marenksa <55407239+marenksa@users.noreply.github.com>
2020-10-22 16:13:10 +0200

    ferdigstiller chatboxen, men ikke boten

--- scripts/hovedside.js
+++ scripts/hovedside.js
@@ -11,21 +11,59 @@ function chat() {
 }
 
 
-function displayMessage()
+function displayMessage(caller)
 {
-  var message = document.getElementById("chatbar").value;
-  console.log(message);
+
+  var chatview = document.getElementById("chatview");
+
+  var nameNode = document.createElement("p");
+  var message;
+  var name;
+
+  if (caller === 'user') 
+  {
+    message = document.getElementById("chatbar").value;
+    name = document.createTextNode("Du: ");
+    nameNode.setAttribute("class", "userChatID");
+    // clearer chatboxen :))
+    document.getElementById("chatbar").value = "";
+  }
+  else if (caller === "bot")
+  {
+    message = chatBotMessage();
+    name = document.createTextNode("Match: ");
+    nameNode.setAttribute("class", "botChatID");
+  }
+  else { console.log("unknown caller of displayMessage(), pls check ur shit"); }
+
+  nameNode.appendChild(name);
+
+  var messageNode = document.createElement("p");
+  var textNode = document.createTextNode(message);
+  messageNode.appendChild(textNode);
+  messageNode.setAttribute("class", "chatMessage");
+
+  chatview.appendChild(nameNode);
+  chatview.appendChild(messageNode);
+  chatview.appendChild(document.createElement("br"));
+  // scroller til bunn av chat på ny mld
+  chatview.scrollTop = chatview.scrollHeight;
+
+  if (caller === "user") { displayMessage("bot"); }
 }
 
+// returnerer en random chat-string
 function chatBotMessage()
 {
-  /* Genererer og displayer chatbot-melding på random elns */ 
+  messages = ['hei', 'du er kul', 'vil du booke et grupperom sammen?'];
+  return messages[Math.round(Math.random()*(messages.length-1))];
 }
 
 document.getElementById("chatbar").addEventListener("keydown", function(key) {
   if(key.keyCode == 13){
       key.preventDefault();
-      displayMessage();
-      document.getElementById("chatbar").value = "";
+      displayMessage('user');
   }
 });
+
+
